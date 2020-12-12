1e7f9b14784bbbd39197ef6cb657d01b241e1cc1
patrick-rush <patrick.a.rush@gmail.com>
2020-12-12 18:23:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 10 % 6;
+
+let max = Math.max(5, 10, 15, 20);
